e01f17acb3e25c037e014c14684ba049f368a414
Evan Davis <etdavis3652@gmail.com>
2025-12-15 21:11:02 -0500

    just testing

--- Frontend/src/app/page.js
+++ Frontend/src/app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(newTask)
-    });
+    }); 
     */
   }
   
